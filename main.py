5e042e8898da62ef2d878a0056bedd8f08ebd842
AkshatAggarwal14 <akshataggarwal1411@gmail.com>
2022-02-13 23:24:13 +0530

    Updated

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import statements
 import time
 from fastapi.openapi.utils import get_openapi
-app = FastAPI(title="Codeforces API", redoc_url="/")
+app = FastAPI(title="Statements API", redoc_url="/")
 
 
 @app.get("/statement")
@@ -15,8 +15,8 @@ def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
     openapi_schema = get_openapi(
-        title="Codeforces API",
-        version="1.1.0",
+        title="Statements API",
+        version="1.1",
         description="",
         routes=app.routes,
     )
--- statements.py
+++ statements.py
@@ -1,21 +1,21 @@
+from attr import attr, attrs
 from bs4 import BeautifulSoup
-import requests
-import json
 import aiohttp
-import re
-from pylatexenc.latex2text import LatexNodes2Text as l2t
 
 
-async def parse_id(id: str) -> list:
-    c_id = ""
-    cnt = 0
-    for c in id:
-        if c.isalpha():
-            break
-        cnt += 1
-        c_id += c
-    p_id = id[-len(id)+cnt:]
-    return [c_id, p_id]
+async def prettify(s: str):
+    return (' '.join(s.split())).strip()
+
+
+async def get(which_soup, to_find, attrs_: dict = {}, index=None, getList=False):
+
+    divs = [div.find(text=True, recursive=False)
+            for div in which_soup.find_all(to_find, attrs=attrs_)]
+    if getList:
+        return divs
+    if index != None:
+        return divs[index]
+    return ' '.join(divs)
 
 
 async def parse_statement(c_id: str, p_id: str):
@@ -26,19 +26,41 @@ async def parse_statement(c_id: str, p_id: str):
                 p_url = f'http://codeforces.com/contest/{c_id}/problem/{p_id}'
                 async with session.get(p_url, allow_redirects=False) as page:
                     if page.status == 200:
-                        page = await page.text()
-                        soup = BeautifulSoup(page, 'html.parser')
-                        divs = soup.find_all('div')
-                        # for i in range(len(divs)):
-                        # print(i, ':', divs[i], '\n')
-                        stuff = [75, 76, 78, 81, 83, 84, 85, 87, 92, 94, 96]
-                        res = []
-                        for i in stuff:
-                            res.append(l2t(strict_latex_spaces=True).latex_to_text(
-                                divs[i].text))
-                        for line in res:
-                            print(line, file=open('output.txt',
-                                  mode='a', encoding='utf-8'))
-                        return res
+                        html = await page.read()
+                        page.close()
+                        resp = {}
+                        soup = BeautifulSoup(html, 'html.parser')
+                        resp['title'] = await get(soup, 'div', {'class': 'title'}, 0)
+                        resp['time_limit'] = await get(soup, 'div', {'class': 'time-limit'})
+                        resp['memory_limit'] = await get(soup, 'div', {'class': 'memory-limit'})
+                        resp['input'] = await get(soup, 'div', {'class': 'input-file'})
+                        resp['output'] = await get(soup, 'div', {'class': 'output-file'})
 
-# 75+
+                        for div in soup.find_all('div', {'class': 'problem-statement'}):
+                            for paras in div.find_all('div', {'class': None}):
+                                resp['statement'] = [
+                                    await prettify(para.text) for para in paras]
+
+                        for paras in soup.find_all('div', {'class': 'input-specification'}):
+                            resp['input_specification'] = [
+                                await prettify(para.text) for para in paras.find_all('p')]
+
+                        for paras in soup.find_all('div', {'class': 'output-specification'}):
+                            resp['output_specification'] = [
+                                await prettify(para.text) for para in paras.find_all('p')]
+
+                        sample_ins = []
+                        for div in soup.find_all('div', {'class': 'input'}):
+                            for input in div.find_all('pre'):
+                                sample_ins.append(
+                                    input.text.strip().splitlines())
+                        resp['sample_input'] = sample_ins
+
+                        sample_outs = []
+                        for div in soup.find_all('div', {'class': 'output'}):
+                            for output in div.find_all('pre'):
+                                sample_outs.append(
+                                    input.text.strip().splitlines())
+                        resp['sample_output'] = sample_outs
+
+                        return resp
